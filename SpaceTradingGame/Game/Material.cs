4f525540978e4965041a351c6aaec2da40970e5a
Trevor Fisher <minalear@gmail.com>
2017-04-27 22:48:06 -0500

    Trade goods (material) class basic outlining.

---
+++ SpaceTradingGame/Game/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace SpaceTradingGame.Game
+{
+    /// <summary>
+    /// Materials generated and traded for money or other materials.  Trade goods.
+    /// </summary>
+    public class Material
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double BaseValue { get; set; }
+        public double Weight { get; set; }
+    }
+}
