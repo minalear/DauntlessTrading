d70876a8c2bf186bcaeeae30bb61c54fb62f22cf
Trevor Fisher <minalear@gmail.com>
2017-06-04 21:45:03 -0500

    Prototyping the system screen.

--- SpaceTradingGame/Engine/UI/Interfaces/SystemScreen.cs
+++ SpaceTradingGame/Engine/UI/Interfaces/SystemScreen.cs
@@ -8,17 +8,63 @@ namespace SpaceTradingGame.Engine.UI.Interfaces
 {
     public class SystemScreen : Interface
     {
+        private int systemOffset = 0;
+
         public SystemScreen(InterfaceManager manager)
             : base(manager)
         {
+            Slider slider = new Slider(null, 0, GraphicConsole.BufferHeight - 2, GraphicConsole.BufferWidth - 1);
 
+            slider.ValueChanged += (sender, e) =>
+            {
+                systemOffset = -(int)(e * 100);
+            };
+
+            RegisterControl(slider);
+
+            genPlanets();
         }
 
         public override void DrawStep()
         {
-            GraphicConsole.Draw.Circle(GraphicConsole.BufferWidth / 2, GraphicConsole.BufferHeight / 2, 10, '*');
+            for (int i = 0; i < 15; i++)
+                GraphicConsole.Draw.Circle(systemOffset, GraphicConsole.BufferHeight / 2, i, '.');
+            GraphicConsole.Draw.Circle(systemOffset, GraphicConsole.BufferHeight / 2, 15, '*');
+
+            int r = 15;
+            int offset = 0;
+            for (int i = 0; i < planets.Count; i++)
+            {
+                offset += r + 6;
+                r = planets[i].R;
+                offset += r;
+
+                GraphicConsole.SetColor(planets[i].Color, Color4.Black);
+                GraphicConsole.Draw.Circle(systemOffset + offset, GraphicConsole.BufferHeight / 2, r, '·');
+            }
+
+            GraphicConsole.ClearColor();
 
             base.DrawStep();
         }
+
+        private void genPlanets()
+        {
+            Color4[] colors = new Color4[] { Color4.Blue, Color4.Red, Color4.Yellow, Color4.Cyan };
+            int num = 8;
+
+            planets = new List<Planet>();
+            for (int i = 0; i < num; i++)
+            {
+                planets.Add(new Planet() { R = RNG.Next(2, 10), Color = colors[RNG.Next(0, colors.Length)] });
+            }
+        }
+
+        struct Planet
+        {
+            public int R;
+            public Color4 Color;
+        }
+        private List<Planet> planets;
     }
 }
